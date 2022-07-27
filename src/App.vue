b0b2f3ec9e908c0cec351cdbe9283fd3aafb0d24
Broderick Lemke <blemke4@gmail.com>
2022-07-27 09:19:23 -0500

    Adding Tilesets

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
     <button @click="setTiles">Change Tiles</button>
     <button @click="increaseSize">Bigger</button>
     <button @click="decreaseSize">Smaller</button>
+    <h2 v-if="tilesets[tilesetIndex] && tilesets[tilesetIndex].name">
+      {{ tilesets[tilesetIndex].name }}
+    </h2>
     <div id="grid" :class="{ rotate: rotate }">
       <div class="row" v-for="(row, index) in grid.contents" :key="index">
         <div class="cell" v-for="(cell, index) in row" :key="index">
@@ -38,15 +41,16 @@ export default {
       grid: {
         contents: [],
       },
-
       tiles: [],
       tileset: "land",
-      landTiles: [],
       timeoutID: undefined,
       rotate: false,
+      tilesets: [],
+      tilesetIndex: 0,
     };
   },
   mounted() {
+    this.createTilesets();
     this.setTiles();
   },
   methods: {
@@ -69,46 +73,137 @@ export default {
         this.createGrid();
       }
     },
-    setTiles: function () {
-      if (this.tileset == "land") {
-        this.tileset = "pipes";
-        const pipeTiles = [
-          new Tile("tile0", new Sockets("BBB", "BBB", "BBB", "BBB")),
-          new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 0),
-          new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 1),
-          new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 2),
-          new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 3),
-        ];
-        this.tiles = pipeTiles;
-      } else {
-        this.tileset = "land";
+    createTilesets: function () {
+      const blockTiles = [
+        new Tile("tile0", new Sockets("BBB", "BBB", "BBB", "BBB")),
+        new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 0),
+        new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 1),
+        new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 2),
+        new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 3),
+      ];
+      this.tilesets.push({ name: "Blocks", tiles: blockTiles });
 
-        const landTiles = [
-          new Tile("tileA", new Sockets("ABB", "BBB", "BBA", "AAA"), 0),
-          new Tile("tileA", new Sockets("ABB", "BBB", "BBA", "AAA"), 1),
-          new Tile("tileA", new Sockets("ABB", "BBB", "BBA", "AAA"), 2),
-          new Tile("tileA", new Sockets("ABB", "BBB", "BBA", "AAA"), 3),
-          new Tile("tileB", new Sockets("AAA", "AAA", "AAA", "AAA")),
-          new Tile("tileC", new Sockets("BBB", "BBB", "BBB", "BBB")), // Lots of C for weighting
-          new Tile("tileC", new Sockets("BBB", "BBB", "BBB", "BBB")),
-          new Tile("tileC", new Sockets("BBB", "BBB", "BBB", "BBB")),
-          new Tile("tileC", new Sockets("BBB", "BBB", "BBB", "BBB")),
-          new Tile("tileC", new Sockets("BBB", "BBB", "BBB", "BBB")),
-          new Tile("tileD", new Sockets("ABB", "BBA", "AAA", "AAA"), 0),
-          new Tile("tileD", new Sockets("ABB", "BBA", "AAA", "AAA"), 1),
-          new Tile("tileD", new Sockets("ABB", "BBA", "AAA", "AAA"), 2),
-          new Tile("tileD", new Sockets("ABB", "BBA", "AAA", "AAA"), 3),
-          new Tile("tileE", new Sockets("BBB", "BBB", "BBA", "ABB"), 0),
-          new Tile("tileE", new Sockets("BBB", "BBB", "BBA", "ABB"), 1),
-          new Tile("tileE", new Sockets("BBB", "BBB", "BBA", "ABB"), 2),
-          new Tile("tileE", new Sockets("BBB", "BBB", "BBA", "ABB"), 3),
-        ];
-        this.tiles = landTiles;
+      const elbowTiles = [
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 0),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 1),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 2),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 3),
+      ];
+      this.tilesets.push({ name: "Elbows", tiles: elbowTiles });
+
+      const knotsTiles = [
+        new Tile("tile0", new Sockets("BBB", "BBB", "BBB", "BBB")),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 0),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 1),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 2),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 3),
+        new Tile("tile3", new Sockets("BAB", "BAB", "BAB", "BAB"), 0),
+        new Tile("tile3", new Sockets("BAB", "BAB", "BAB", "BAB"), 1), // Overlapping tile rotation
+        new Tile("tile7", new Sockets("BBB", "BAB", "BBB", "BAB"), 0),
+        new Tile("tile7", new Sockets("BBB", "BAB", "BBB", "BAB"), 1),
+      ];
+      this.tilesets.push({ name: "Knots", tiles: knotsTiles });
+
+      const tighterKnots = [
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 0),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 1),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 2),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 3),
+        new Tile("tile3", new Sockets("BAB", "BAB", "BAB", "BAB"), 0),
+        new Tile("tile3", new Sockets("BAB", "BAB", "BAB", "BAB"), 1), // Overlapping tile rotation
+        new Tile("tile7", new Sockets("BBB", "BAB", "BBB", "BAB"), 0),
+        new Tile("tile7", new Sockets("BBB", "BAB", "BBB", "BAB"), 1),
+      ];
+      this.tilesets.push({ name: "Tighter Knots", tiles: tighterKnots });
+
+      const weaveTiles = [
+        new Tile("tile3", new Sockets("BAB", "BAB", "BAB", "BAB"), 0),
+        new Tile("tile3", new Sockets("BAB", "BAB", "BAB", "BAB"), 1),
+        new Tile("tile7", new Sockets("BBB", "BAB", "BBB", "BAB"), 0),
+        new Tile("tile7", new Sockets("BBB", "BAB", "BBB", "BAB"), 1),
+      ];
+      this.tilesets.push({ name: "Weave", tiles: weaveTiles });
+
+      const funkyTiles = [
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 0),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 1),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 2),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 3),
+        new Tile("tile5", new Sockets("BAB", "BAB", "BBB", "BBB"), 0),
+        new Tile("tile5", new Sockets("BAB", "BAB", "BBB", "BBB"), 1),
+        new Tile("tile5", new Sockets("BAB", "BAB", "BBB", "BBB"), 2),
+        new Tile("tile5", new Sockets("BAB", "BAB", "BBB", "BBB"), 3),
+        new Tile("tile6", new Sockets("BAB", "BAB", "BAB", "BAB"), 0),
+        new Tile("tile6", new Sockets("BAB", "BAB", "BAB", "BAB"), 1),
+        new Tile("tile7", new Sockets("BBB", "BAB", "BBB", "BAB"), 0),
+        new Tile("tile7", new Sockets("BBB", "BAB", "BBB", "BAB"), 1),
+      ];
+      this.tilesets.push({ name: "Funky", tiles: funkyTiles });
+
+      const allTiles = [
+        new Tile("tile0", new Sockets("BBB", "BBB", "BBB", "BBB")),
+        new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 0),
+        new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 1),
+        new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 2),
+        new Tile("tile1", new Sockets("BBB", "BAB", "BAB", "BAB"), 3),
+        new Tile("tile3", new Sockets("BAB", "BAB", "BAB", "BAB"), 0),
+        new Tile("tile3", new Sockets("BAB", "BAB", "BAB", "BAB"), 1),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 0),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 1),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 2),
+        new Tile("tile4", new Sockets("BAB", "BAB", "BBB", "BBB"), 3),
+        new Tile("tile5", new Sockets("BAB", "BAB", "BBB", "BBB"), 0),
+        new Tile("tile5", new Sockets("BAB", "BAB", "BBB", "BBB"), 1),
+        new Tile("tile5", new Sockets("BAB", "BAB", "BBB", "BBB"), 2),
+        new Tile("tile5", new Sockets("BAB", "BAB", "BBB", "BBB"), 3),
+        new Tile("tile6", new Sockets("BAB", "BAB", "BAB", "BAB"), 0),
+        new Tile("tile6", new Sockets("BAB", "BAB", "BAB", "BAB"), 1),
+        new Tile("tile7", new Sockets("BBB", "BAB", "BBB", "BAB"), 0),
+        new Tile("tile7", new Sockets("BBB", "BAB", "BBB", "BAB"), 1),
+      ];
+      this.tilesets.push({ name: "All", tiles: allTiles });
+
+      const landTiles = [
+        new Tile("tileA", new Sockets("ABB", "BBB", "BBA", "AAA"), 0),
+        new Tile("tileA", new Sockets("ABB", "BBB", "BBA", "AAA"), 1),
+        new Tile("tileA", new Sockets("ABB", "BBB", "BBA", "AAA"), 2),
+        new Tile("tileA", new Sockets("ABB", "BBB", "BBA", "AAA"), 3),
+        new Tile("tileB", new Sockets("AAA", "AAA", "AAA", "AAA")),
+        new Tile("tileC", new Sockets("BBB", "BBB", "BBB", "BBB")), // Lots of C for weighting
+        new Tile("tileC", new Sockets("BBB", "BBB", "BBB", "BBB")),
+        new Tile("tileC", new Sockets("BBB", "BBB", "BBB", "BBB")),
+        new Tile("tileC", new Sockets("BBB", "BBB", "BBB", "BBB")),
+        new Tile("tileC", new Sockets("BBB", "BBB", "BBB", "BBB")),
+        new Tile("tileD", new Sockets("ABB", "BBA", "AAA", "AAA"), 0),
+        new Tile("tileD", new Sockets("ABB", "BBA", "AAA", "AAA"), 1),
+        new Tile("tileD", new Sockets("ABB", "BBA", "AAA", "AAA"), 2),
+        new Tile("tileD", new Sockets("ABB", "BBA", "AAA", "AAA"), 3),
+        new Tile("tileE", new Sockets("BBB", "BBB", "BBA", "ABB"), 0),
+        new Tile("tileE", new Sockets("BBB", "BBB", "BBA", "ABB"), 1),
+        new Tile("tileE", new Sockets("BBB", "BBB", "BBA", "ABB"), 2),
+        new Tile("tileE", new Sockets("BBB", "BBB", "BBA", "ABB"), 3),
+      ];
+      this.tilesets.push({ name: "Land", tiles: landTiles });
+    },
+    setTiles: function () {
+      // Increase index, go back to 0 if needed.
+      this.tilesetIndex += 1;
+
+      if (this.tilesetIndex >= this.tilesets.length) {
+        this.tilesetIndex = 0;
       }
+
+      // set tiles
+      this.tiles = this.tilesets[this.tilesetIndex].tiles;
+
       this.createGrid();
     },
     createGrid: function () {
-      this.grid = new Grid(this.width, this.height, this.tiles);
+      this.grid = new Grid(
+        this.width,
+        this.height,
+        this.tilesets[this.tilesetIndex].tiles
+      );
     },
     fillGrid: function () {
       if (!this.grid.complete()) {
@@ -162,6 +257,9 @@ export default {
   }
 }
 
+h2 {
+  text-align: center;
+}
 .cell {
   border: 1px solid black;
   aspect-ratio: auto 1;
